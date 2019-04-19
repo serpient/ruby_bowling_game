c97c3d50013b75a1d7b727995e91c9186376b15f
serpient <littlegangwolf@gmail.com>
2019-04-19 08:50:23 -0700

    setup iteration 4 + 5 files

---
+++ lib/bowling_5.rb
@@ -0,0 +1,8 @@
+class Bowling3
+    attr_accessor :rolls, :strike, :miss, :spare
+end
+
+
+
+
+# value[/\d/].to_i
---
+++ spec/bowling_5_spec.rb
@@ -0,0 +1,24 @@
+require_relative '../lib/bowling_2'
+
+describe 'bowling3' do
+
+end
+
+
+# strike_set = 'X X X X X X X X X X X X' 
+# 300
+
+# miss_set = '9- 9- 9- 9- 9- 9- 9- 9- 9- 9- ' 
+# 90
+
+# spare_set = '5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5' 
+# 150
+
+# mixed_set = '5- 8/ 9- 9/ 9- 5/ 9- 9- 3/ 4-' 
+# 116
+
+# mixed_set = '5- 8/ 9- 9/ 9- 5/ 9- 9- 3/ 4/5' 
+# 127
+
+# mixed_set = '3/ 6/ 5/ 8/ 2/ 4/ 9/ 4/ 1/ 0/9' 
+# 148
