1b403a2aebf3afbe5e6ed778d861bf4c8258fe4e
serpient <littlegangwolf@gmail.com>
2019-04-19 09:18:08 -0700

    add more starter test cases for iteration 5

--- spec/bowling_5_spec.rb
+++ spec/bowling_5_spec.rb
@@ -4,6 +4,45 @@ describe 'bowling3' do
 
 end
 
+#initialization
+# set = 'X 3- 5/'
+# ['X', '3-', '5/']
+
+# miss
+# miss = '3-'
+# 3
+
+# miss = '9-'
+# 9
+
+
+# spares
+# spare_set = '9/ 3-'
+# 13
+
+# spare_set = '9/ 5/'
+# 15
+
+# spare_set = '9/ X'
+# 20
+
+# spare_set = '9/4'
+# 14
+
+
+# strikes
+# strike_set = 'X 3- 5/'
+# 18
+
+# strike_set = 'X X X'
+# 30
+
+# strike_set = 'X 6/3'
+# 19
+
+# invalid_strike_set = 'X'
+# 0
+
 
 # strike_set = 'X X X X X X X X X X X X' 
 # 300
