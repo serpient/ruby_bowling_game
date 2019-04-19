5fa4e210e5f09825065348533e6f66dc41885dd5
serpient <littlegangwolf@gmail.com>
2019-04-19 09:16:45 -0700

    finished iteration 4 in 16 minutes

--- lib/bowling_4.rb
+++ lib/bowling_4.rb
@@ -1,8 +1,54 @@
-class Bowling3
+class Bowling4
     attr_accessor :rolls, :strike, :miss, :spare
+
+    def initialize(rolls)
+        @rolls = rolls.split
+        @strike = 'X'
+        @miss = '-'
+        @spare = '/'
+    end
+
+    def parse(value) 
+        value == strike ? 10 : value[/\d/].to_i
+    end
+
+    def next_roll(idx)
+        rolls[idx + 1]
+    end
+
+    def score_miss(idx)
+        parse(rolls[idx])
+    end
+
+    def score_spare(idx)
+        return 10 + parse(rolls[idx][-1]) unless next_roll(idx)
+        10 + parse(next_roll(idx))
+    end
+
+    def score_strike(idx)
+        if next_roll(idx) and next_roll(idx + 1)
+            return 10 + parse(next_roll(idx)) + parse(next_roll(idx + 1))
+        elsif next_roll(idx) and next_roll(idx).include? spare
+            return 10 + parse(next_roll(idx)) + parse(next_roll(idx)[-1])
+        else
+            0
+        end
+    end
+
+    def score()
+        score = 0
+        rolls.each_with_index do |roll, idx|
+            score += score_strike(idx) if roll.include? strike
+            score += score_miss(idx) if roll.include? miss
+            score += score_spare(idx) if roll.include? spare
+        end
+        score
+    end
 end
 
 
 
 
 # value[/\d/].to_i
+
+# took 16 minutes
--- spec/bowling_4_spec.rb
+++ spec/bowling_4_spec.rb
@@ -1,8 +1,133 @@
-require_relative '../lib/bowling_2'
+require_relative '../lib/bowling_4'
 
-describe 'bowling3' do
+describe 'bowling4' do
+    it 'initializes new bowling with converted rolls' do
+        set = 'X 3- 5/'
+        bowling = Bowling4.new(set)
+        expect(bowling.rolls).to eq ['X', '3-', '5/']
+    end
 
+    context 'scores miss'  do
+        it '3' do
+            miss = '3-'
+            bowling = Bowling4.new(miss)
+            expect(bowling.score_miss(0)).to eq 3
+        end
+
+        it '5' do
+            miss = '5-'
+            bowling = Bowling4.new(miss)
+            expect(bowling.score_miss(0)).to eq 5
+        end
+    end 
+
+    context 'scores spare'  do
+        it '13' do
+            spare = '3/ 3-'
+            bowling = Bowling4.new(spare)
+            expect(bowling.score_spare(0)).to eq 13
+        end
+
+        it '15' do
+            spare_set = '9/ 5/'
+            bowling = Bowling4.new(spare_set)
+            expect(bowling.score_spare(0)).to eq 15
+        end
+
+        it '20' do
+            spare_set = '9/ X'
+            bowling = Bowling4.new(spare_set)
+            expect(bowling.score_spare(0)).to eq 20
+        end
+
+        it '14' do
+            spare_set = '9/4'
+            bowling = Bowling4.new(spare_set)
+            expect(bowling.score_spare(0)).to eq 14
+        end
+    end 
+
+    context 'scores strikes'  do
+        it '18' do
+            strike_set = 'X 3- 5/'
+            bowling = Bowling4.new(strike_set)
+            expect(bowling.score_strike(0)).to eq 18
+        end
+
+        it '30' do
+            strike_set = 'X X X'
+            bowling = Bowling4.new(strike_set)
+            expect(bowling.score_strike(0)).to eq 30
+        end
+
+        it '19' do
+            strike_set = 'X 6/3'
+            bowling = Bowling4.new(strike_set)
+            expect(bowling.score_strike(0)).to eq 19
+        end
+
+        it '0' do
+            strike_set = 'X'
+            bowling = Bowling4.new(strike_set)
+            expect(bowling.score_strike(0)).to eq 0
+        end
+    end 
+
+    context 'scores mix set'  do
+        it '300' do
+            strike_set = 'X X X X X X X X X X X X' 
+            bowling = Bowling4.new(strike_set)
+            expect(bowling.score()).to eq 300
+        end
+
+        it '90' do
+            miss_set = '9- 9- 9- 9- 9- 9- 9- 9- 9- 9- ' 
+            bowling = Bowling4.new(miss_set)
+            expect(bowling.score()).to eq 90
+        end
+
+        it '150' do
+            spare_set = '5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5' 
+            bowling = Bowling4.new(spare_set)
+            expect(bowling.score()).to eq 150
+        end
+
+        it '116' do
+            mixed_set = '5- 8/ 9- 9/ 9- 5/ 9- 9- 3/ 4-' 
+            bowling = Bowling4.new(mixed_set)
+            expect(bowling.score()).to eq 116
+        end
+    end
 end
+
+
+
+# spares
+# spare_set = '9/ 3-'
+# 13
+
+# spare_set = '9/ 5/'
+# 15
+
+# spare_set = '9/ X'
+# 20
+
+# spare_set = '9/4'
+# 14
+
+
+# strikes
+# strike_set = 'X 3- 5/'
+# 18
+
+# strike_set = 'X X X'
+# 30
+
+# strike_set = 'X 6/3'
+# 19
+
+# invalid_strike_set = 'X'
+# 0
 
 
 # strike_set = 'X X X X X X X X X X X X' 
